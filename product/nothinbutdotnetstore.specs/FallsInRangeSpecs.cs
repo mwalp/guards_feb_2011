33da6e8a1d76b33f51b19ce845b770d822f3624a
mwalp <mwalp@gpsx.net>
2011-02-25 14:36:13 -0500

    Pushing new changes

---
+++ product/nothinbutdotnetstore.specs/FallsInRangeSpecs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure;
+using nothinbutdotnetstore.infrastructure.validation.attributes;
+using Rhino.Mocks;
+
+namespace nothinbutdotnetstore.specs
+{
+    class FallsInRangeSpecs
+    {
+        public abstract class when_validating_an_property : Observes<FallsInRangeAttribute>
+        {
+            Establish c = () =>
+            {
+                item = new ItemToValidate();
+                item.PropertyToValidate = 19;
+                item_property = an<ItemProperty>();
+                item_property.Stub(x => x.get_value_from(item)).Return(item.PropertyToValidate);
+                
+                create_sut_using(()=> {return new FallsInRangeAttribute(10,20);});
+            };
+
+            Because b = () =>
+                result = sut.is_valid(item);
+
+            It should_return_true_if_the_property_value_is_in_range = () =>
+                                                                                  {
+                                                                                      result.ShouldBeTrue();
+                                                                                  };
+
+
+
+            static PropertyInfo property_info;
+            private static ItemProperty item_property;
+            private static ItemToValidate item;
+            private static bool result;
+        }
+        
+        public class ItemToValidate
+        {
+            public int PropertyToValidate { get; set; }
+        }
+    }
+}
--- product/nothinbutdotnetstore/infrastructure/validation/attributes/FallsInRangeAttribute.cs
+++ product/nothinbutdotnetstore/infrastructure/validation/attributes/FallsInRangeAttribute.cs
@@ -7,11 +7,20 @@ namespace nothinbutdotnetstore.infrastructure.validation.attributes
     {
         int start;
         int end;
-
+        private ItemProperty property { get; set; }
+        
         public FallsInRangeAttribute(int start,int end)
         {
             this.start = start;
             this.end = end;
         }
+
+        public bool is_valid(object item)
+        {
+            int value= (int)property.get_value_from(item);
+            if (value >= start && value <= end) return true;
+            return false;
+        }
+
     }
 }
